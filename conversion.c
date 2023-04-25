bc3fbd8be1b1c261fb71742f66adf461bdec14c1
AymanAegon <ayo12ammar24@gmail.com>
2023-04-25 15:07:58 -0700

    Handle the following conversion specifiers d and i

--- conversion.c
+++ conversion.c
@@ -9,19 +9,24 @@ int check_print(va_list ptr, char c)
 {
 	char *str;
 
-	switch (c)
+	if (c == '%')
 	{
-		case '%':
-			print_char('%');
-			return (1);
-		case 'c':
-			print_char(va_arg(ptr, int));
-			return (1);
-		case 's':
-			str = va_arg(ptr, char *);
-			print_string(str);
-			return (strlen(str));
-		default:
-			return (0);
+		_putchar('%');
+		return (1);
 	}
+	else if (c == 'c')
+	{
+		_putchar(va_arg(ptr, int));
+		return (1);
+	}
+	else if (c == 's')
+	{
+		str = va_arg(ptr, char *);
+		print_string(str);
+		return (strlen(str));
+	}
+	else if (c == 'i' || c == 'd')
+		return (print_int(va_arg(ptr, int)));
+	else
+		return (0);
 }
